047c83e99e8db4d3e7eb308fcd78a4aef964ccd5
Balogun Silver <silverg33k@gmail.com>
2021-03-01 16:01:40 +0100

    ...

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -4,7 +4,6 @@ const router = express.Router()
 router.use(express.json())
 router.use(express.urlencoded({ extended: true }))
 
-
 router.route('/send').post((request, response) => {
     response.send('send okay')
 })
